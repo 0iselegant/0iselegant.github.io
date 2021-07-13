cfd59bfadb8d5cc2446bdbd0f5656778add37c51
0iselegant <73006909+0iselegant@users.noreply.github.com>
2021-07-13 21:02:52 +1000

    Update main.js

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ navbar.onclick = function(event) {
             event.target.onmouseover = null;
             event.target.onmouseout = null;
             element.style.display = 'block';
-            element.style.position = 'fixed';
+            element.style.position = 'absolute';
         }
         return;
     }
